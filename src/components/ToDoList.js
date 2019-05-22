1416e5760193c452b3a088a97e98b9532067b03c
Erin Gurley <erinmgurley@gmail.com>
2019-05-21 21:00:10 -0700

    populated todo list data, app component initialization, added a div section to the html

---
+++ src/components/ToDoList.js
@@ -0,0 +1,19 @@
+const todos = [
+    {
+        task: 'Homework',
+        label: 'homework',
+        completed: true
+    }, 
+    {   
+        task: 'Make Lunch',
+        label: 'make-lunch',
+        completed: true
+    },
+    {   
+        task: 'Clean House',
+        label: 'clean-house',
+        completed: false
+    },
+];
+
+export default todos;
