8b373d91d1065d25b6cb61ac2c0189551358fec8
Erin Gurley <erinmgurley@gmail.com>
2019-05-21 20:47:17 -0700

    set up all config files and set up file structure

---
+++ src/components/component.js
@@ -0,0 +1,30 @@
+import htmlToDOM from './src/html-to-DOM.js';
+
+class Component {
+    constructor(props) {
+        this.props = props;
+    }
+    
+    render() {
+        return this.renderDom();
+    }
+
+    renderDom() {
+        const html = this.renderTemplate();
+        const dom = htmlToDOM(html);
+        this.rootElement = dom;
+        return dom;
+    }
+
+    update(props) {
+        // update the props:
+        Object.keys(props).forEach(key => {
+            this.props[key] = props[key];
+        });
+        const oldRoot = this.rootElement;
+        const newDOM = this.render();
+        oldRoot.replaceWith(newDOM);
+    }
+}
+
+export default Component;
